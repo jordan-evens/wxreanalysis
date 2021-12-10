8c61580706940e23f4dcba501e690fb0d399a77e
Jordan Evens <jordan.evens@canada.ca>
2021-12-10 13:34:46 -0500

    got hourly weather for a year to download

--- wx_era5.py
+++ wx_era5.py
@@ -24,61 +24,105 @@ def to_datetime(hours):
     return(datetime.datetime(1900, 1, 1) + datetime.timedelta(hours=hours))
 
 
-MONTHS = list(map(lambda x: '{:02d}'.format(x), range(1, 13))),
-DAYS = list(map(lambda x: '{:02d}'.format(x), range(1, 32))),
-
-def get_nc(variables,
-           year,
-           pressure_level = None,
-           months=MONTHS,
-           days=DAYS,
-           hours=[12],
-           level_type='reanalysis-era5-pressure-levels'):
-    nc_file = '{:04d}_{:s}.nc'.format(year,
-                                      '{:04d}'.format(pressure_level) if pressure_level is not None else "sfc")
-    if not os.path.exists(nc_file):
-        args = {
-            'product_type': 'reanalysis',
-            'variable': variables,
-            'year': '{:04d}'.format(year),
-            'month': MONTHS,
-            'day': DAYS,
-            'time': list(map(lambda x: '{:02d}:00'.format(x), hours)),
-            'grid': [1.0, 1.0],
-            'area': AREA,
-            'format': 'netcdf',  # Supported format: grib and netcdf. Default: grib
-        }
-        if pressure_level is not None:
-            args['pressure_level'] = str(pressure_level)
-        c.retrieve(
-            level_type,
-            args,
-            nc_file)  # Output file. Adapt as you wish.
-    return(nc_file)
-
-get_nc(['temperature', 'relative_humidity'], 2008, 12, 2)
-get_nc(['u_component_of_wind' 'v_component_of_wind'], 2008, 12, 10)
-get_nc('total_precipitation', 2008, 12, hours=range(0, 24), level_type='reanalysis-era5-single-levels')
-
-data = netCDF4.Dataset(nc_file)
-start_date = to_datetime(int(data.variables['time'][[0]].data[0]))
-key = list(data.variables.keys())[3]
-df = pd.DataFrame(data=None, columns=["date", "latitude", "longitude", key])
-df.to_csv(csv_file, index=False)
-#result = []
-for i_time, time in enumerate(data.variables['time']):
-    date = to_datetime(int(time.data))
-    print(date)
-    by_latitude = []
-    for i_lat, latitude in enumerate(data.variables['latitude']):
-        longitudes = data.variables['longitude'][:].data
-        values = data.variables[key][i_time, i_lat].data
-        df = pd.DataFrame({'longitude': longitudes, variable: values})
-        df['latitude'] = latitude
-        by_latitude.append(df)
-    df = pd.concat(by_latitude)
-    df['date'] = date
-    #result.append(df)
-    df = df[['date', 'latitude', 'longitude', variable]]
-    df.to_csv(csv_file, header=False, index=False, mode='a')
-
+MONTHS = list(map(lambda x: '{:02d}'.format(x), range(1, 13)))
+DAYS = list(map(lambda x: '{:02d}'.format(x), range(1, 32)))
+#
+# def get_nc(variable,
+#            year,
+#            pressure_level=None,
+#            months=MONTHS,
+#            days=DAYS,
+#            hours=[12],
+#            level_type='reanalysis-era5-pressure-levels'):
+#     nc_file = '{:04d}_{:s}.nc'.format(year,
+#                                       '{:04d}'.format(pressure_level) if pressure_level is not None else "sfc")
+#     if not os.path.exists(nc_file):
+#         args = {
+#             'product_type': 'reanalysis',
+#             'variable': variable,
+#             'year': '{:04d}'.format(year),
+#             'month': months,
+#             'day': days,
+#             'time': list(map(lambda x: '{:02d}:00'.format(x), hours)),
+#             'area': AREA,
+#             'format': 'netcdf',  # Supported format: grib and netcdf. Default: grib
+#         }
+#         if pressure_level is not None:
+#             args['pressure_level'] = str(pressure_level)
+#         print(args)
+#         c.retrieve(
+#             level_type,
+#             args,
+#             nc_file)  # Output file. Adapt as you wish.
+#     return(nc_file)
+area = "/".join(map(str, AREA))
+time = list(map(lambda x: '{:02d}:00'.format(x), range(0, 24)))
+c.retrieve(
+    'reanalysis-era5-pressure-levels',
+    {
+        'product_type': 'reanalysis',
+        'variable': ['temperature', 'relative_humidity'],
+        'pressure_level': '2',
+        'year': '{:04d}'.format(year),
+        'month': MONTHS,
+        'day': DAYS,
+        'time': time,
+        'area': AREA,
+        'format': 'netcdf',
+    },
+    '{year:04d}_temp_rh.nc'.format(year=year))
+# get_nc('temperature', 2008, 2)
+# get_nc('relative_humidity', 2008, 2)
+c.retrieve(
+    'reanalysis-era5-pressure-levels',
+    {
+        'product_type': 'reanalysis',
+        'variable': ['u_component_of_wind', 'v_component_of_wind'],
+        'pressure_level': '10',
+        'year': '{:04d}'.format(year),
+        'month': MONTHS,
+        'day': DAYS,
+        'time': time,
+        'area': AREA,
+        'format': 'netcdf',
+    },
+    '{year:04d}_wind.nc'.format(year=year))
+# get_nc('u_component_of_wind', 2008, 10)
+# get_nc('v_component_of_wind', 2008, 10)
+c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type': 'reanalysis',
+        'variable': 'total_precipitation',
+        'year': '{:04d}'.format(year),
+        'month': MONTHS,
+        'day': DAYS,
+        'time': time,
+        'area': AREA,
+        'format': 'netcdf',
+    },
+    '{year:04d}_prec.nc'.format(year=year))
+#get_nc('total_precipitation', 2008, hours=range(0, 24), level_type='reanalysis-era5-single-levels')
+#
+# data = netCDF4.Dataset(nc_file)
+# start_date = to_datetime(int(data.variables['time'][[0]].data[0]))
+# key = list(data.variables.keys())[3]
+# df = pd.DataFrame(data=None, columns=["date", "latitude", "longitude", key])
+# df.to_csv(csv_file, index=False)
+# #result = []
+# for i_time, time in enumerate(data.variables['time']):
+#     date = to_datetime(int(time.data))
+#     print(date)
+#     by_latitude = []
+#     for i_lat, latitude in enumerate(data.variables['latitude']):
+#         longitudes = data.variables['longitude'][:].data
+#         values = data.variables[key][i_time, i_lat].data
+#         df = pd.DataFrame({'longitude': longitudes, variable: values})
+#         df['latitude'] = latitude
+#         by_latitude.append(df)
+#     df = pd.concat(by_latitude)
+#     df['time'] = date
+#     #result.append(df)
+#     df = df[['date', 'latitude', 'longitude', variable]]
+#     df.to_csv(csv_file, header=False, index=False, mode='a')
+#
