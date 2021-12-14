444cd234f1267d3b44230c799b83dbbda86c35ad
Jordan Evens <jordan.evens@canada.ca>
2021-12-14 08:54:06 -0500

    downloading and converting netcdf

--- wx_era5.py
+++ wx_era5.py
@@ -5,6 +5,8 @@ import netCDF4
 import datetime
 import os
 import pandas as pd
+import numpy as np
+import math
 
 LATITUDE_MIN = 41
 LATITUDE_MAX = 84
@@ -12,117 +14,206 @@ LONGITUDE_MIN = -141
 LONGITUDE_MAX = -52
 AREA = [LATITUDE_MAX, LONGITUDE_MIN, LATITUDE_MIN, LONGITUDE_MAX]
 # NOTE: need to get a client key as per https://cds.climate.copernicus.eu/api-how-to
+DIR = 'era5'
 
 c = cdsapi.Client()
 
-variable = 'temperature'
-year = 2008
-hour = 12
-csv_file = '{:04d}.csv'.format(year, variable)
+def calc_rh(Td, T):
+    """!
+    Calculate RH based on dewpoint temp and temp
+    @param Td Dewpoint temperature (Celcius)
+    @param T Temperature (Celcius)
+    @return Relative Humidity (%)
+    """
+    m = 7.591386
+    Tn = 240.7263
+    return 100 * pow(10, (m * ((Td / (Td + Tn)) - (T / (T + Tn)))))
 
-def to_datetime(hours):
-    return(datetime.datetime(1900, 1, 1) + datetime.timedelta(hours=hours))
+
+def calc_ws(u, v):
+    """!
+    Calculate wind speed in km/h from U and V wind given in m/s
+    @param u Wind vector in U direction (m/s)
+    @param v Wind vector in V direction (m/s)
+    @return Calculated wind speed (km/h)
+    """
+    # NOTE: convert m/s to km/h
+    return 3.6 * math.sqrt(u * u + v * v)
 
 
-MONTHS = list(map(lambda x: '{:02d}'.format(x), range(1, 13)))
-DAYS = list(map(lambda x: '{:02d}'.format(x), range(1, 32)))
-#
-# def get_nc(variable,
-#            year,
-#            pressure_level=None,
-#            months=MONTHS,
-#            days=DAYS,
-#            hours=[12],
-#            level_type='reanalysis-era5-pressure-levels'):
-#     nc_file = '{:04d}_{:s}.nc'.format(year,
-#                                       '{:04d}'.format(pressure_level) if pressure_level is not None else "sfc")
-#     if not os.path.exists(nc_file):
-#         args = {
-#             'product_type': 'reanalysis',
-#             'variable': variable,
-#             'year': '{:04d}'.format(year),
-#             'month': months,
-#             'day': days,
-#             'time': list(map(lambda x: '{:02d}:00'.format(x), hours)),
-#             'area': AREA,
-#             'format': 'netcdf',  # Supported format: grib and netcdf. Default: grib
-#         }
-#         if pressure_level is not None:
-#             args['pressure_level'] = str(pressure_level)
-#         print(args)
-#         c.retrieve(
-#             level_type,
-#             args,
-#             nc_file)  # Output file. Adapt as you wish.
-#     return(nc_file)
-area = "/".join(map(str, AREA))
-time = list(map(lambda x: '{:02d}:00'.format(x), range(0, 24)))
-c.retrieve(
-    'reanalysis-era5-pressure-levels',
-    {
-        'product_type': 'reanalysis',
-        'variable': ['temperature', 'relative_humidity'],
-        'pressure_level': '2',
-        'year': '{:04d}'.format(year),
-        'month': MONTHS,
-        'day': DAYS,
-        'time': time,
-        'area': AREA,
-        'format': 'netcdf',
-    },
-    '{year:04d}_temp_rh.nc'.format(year=year))
-# get_nc('temperature', 2008, 2)
-# get_nc('relative_humidity', 2008, 2)
-c.retrieve(
-    'reanalysis-era5-pressure-levels',
-    {
-        'product_type': 'reanalysis',
-        'variable': ['u_component_of_wind', 'v_component_of_wind'],
-        'pressure_level': '10',
-        'year': '{:04d}'.format(year),
-        'month': MONTHS,
-        'day': DAYS,
-        'time': time,
-        'area': AREA,
-        'format': 'netcdf',
-    },
-    '{year:04d}_wind.nc'.format(year=year))
-# get_nc('u_component_of_wind', 2008, 10)
-# get_nc('v_component_of_wind', 2008, 10)
-c.retrieve(
-    'reanalysis-era5-single-levels',
-    {
-        'product_type': 'reanalysis',
-        'variable': 'total_precipitation',
-        'year': '{:04d}'.format(year),
-        'month': MONTHS,
-        'day': DAYS,
-        'time': time,
-        'area': AREA,
-        'format': 'netcdf',
-    },
-    '{year:04d}_prec.nc'.format(year=year))
-#get_nc('total_precipitation', 2008, hours=range(0, 24), level_type='reanalysis-era5-single-levels')
-#
-# data = netCDF4.Dataset(nc_file)
-# start_date = to_datetime(int(data.variables['time'][[0]].data[0]))
-# key = list(data.variables.keys())[3]
-# df = pd.DataFrame(data=None, columns=["date", "latitude", "longitude", key])
-# df.to_csv(csv_file, index=False)
-# #result = []
-# for i_time, time in enumerate(data.variables['time']):
-#     date = to_datetime(int(time.data))
-#     print(date)
-#     by_latitude = []
-#     for i_lat, latitude in enumerate(data.variables['latitude']):
-#         longitudes = data.variables['longitude'][:].data
-#         values = data.variables[key][i_time, i_lat].data
-#         df = pd.DataFrame({'longitude': longitudes, variable: values})
-#         df['latitude'] = latitude
-#         by_latitude.append(df)
-#     df = pd.concat(by_latitude)
-#     df['time'] = date
-#     #result.append(df)
-#     df = df[['date', 'latitude', 'longitude', variable]]
-#     df.to_csv(csv_file, header=False, index=False, mode='a')
-#
+def calc_wd(u, v):
+    """!
+    Calculate wind direction from U and V wind given in m/s
+    @param u Wind vector in U direction (m/s)
+    @param v Wind vector in V direction (m/s)
+    @return Wind direction (degrees)
+    """
+    return ((180 / math.pi * math.atan2(-u, -v)) + 360) % 360
+
+
+def kelvin_to_celcius(t):
+    """!
+    Convert temperature in Kelvin to Celcius
+    @param t Temperature (Celcius)
+    @return Temperature (Kelvin)
+    """
+    return t - 273.15
+
+def get_year(year = 2008):
+    csv_file = os.path.join(DIR, '{:04d}.csv'.format(year))
+
+    def to_datetime(hours):
+        return(datetime.datetime(1900, 1, 1) + datetime.timedelta(hours=hours))
+
+    MONTHS = list(map(lambda x: '{:02d}'.format(x), range(1, 13)))
+    DAYS = list(map(lambda x: '{:02d}'.format(x), range(1, 32)))
+    area = "/".join(map(str, AREA))
+    time = list(map(lambda x: '{:02d}:00'.format(x), range(0, 24)))
+    file_by_year = os.path.join(DIR, '{year:04d}.nc'.format(year=year))
+    # file_temp_rh = os.path.join(DIR, '{year:04d}_temp_rh.nc'.format(year=year))
+    file_temp = os.path.join(DIR, '{year:04d}_temp.nc'.format(year=year))
+    file_dew = os.path.join(DIR, '{year:04d}_dew.nc'.format(year=year))
+    # file_wind = os.path.join(DIR, '{year:04d}_wind.nc'.format(year=year))
+    file_wind_u = os.path.join(DIR, '{year:04d}_wind_u.nc'.format(year=year))
+    file_wind_v = os.path.join(DIR, '{year:04d}_wind_v.nc'.format(year=year))
+    file_prec = os.path.join(DIR, '{year:04d}_prec.nc'.format(year=year))
+    if not os.path.exits(file_temp):
+        c.retrieve(
+            'reanalysis-era5-land',
+            {
+                'product_type': 'reanalysis',
+                'variable': '2m_temperature',
+                'year': '{:04d}'.format(year),
+                'month': MONTHS,
+                'day': DAYS,
+                'time': time,
+                'area': AREA,
+                'format': 'netcdf',
+            },
+            file_temp)
+    if not os.path.exists(file_dew):
+        c.retrieve(
+            'reanalysis-era5-land',
+            {
+                'product_type': 'reanalysis',
+                'variable': '2m_dewpoint_temperature',
+                'year': '{:04d}'.format(year),
+                'month': MONTHS,
+                'day': DAYS,
+                'time': time,
+                'area': AREA,
+                'format': 'netcdf',
+            },
+            file_dew)
+    if not os.path.exists(file_wind_u):
+        c.retrieve(
+            'reanalysis-era5-land',
+            {
+                'product_type': 'reanalysis',
+                'variable': '10m_u_component_of_wind',
+                'year': '{:04d}'.format(year),
+                'month': MONTHS,
+                'day': DAYS,
+                'time': time,
+                'area': AREA,
+                'format': 'netcdf',
+            },
+            file_wind_u)
+    if not os.path.exists(file_wind_v):
+        c.retrieve(
+            'reanalysis-era5-land',
+            {
+                'product_type': 'reanalysis',
+                'variable': '10m_v_component_of_wind',
+                'year': '{:04d}'.format(year),
+                'month': MONTHS,
+                'day': DAYS,
+                'time': time,
+                'area': AREA,
+                'format': 'netcdf',
+            },
+            file_wind_v)
+    if not os.path.exists(file_prec):
+        c.retrieve(
+            'reanalysis-era5-land',
+            {
+                'product_type': 'reanalysis',
+                'variable': ['total_precipitation'],
+                'year': '{:04d}'.format(year),
+                'month': MONTHS,
+                'day': DAYS,
+                'time': time,
+                'area': AREA,
+                'format': 'netcdf',
+            },
+            file_prec)
+
+    nc_temp = netCDF4.Dataset(file_temp)
+    nc_dew = netCDF4.Dataset(file_dew)
+    nc_wind_u = netCDF4.Dataset(file_wind_u)
+    nc_wind_v = netCDF4.Dataset(file_wind_v)
+    nc_prec = netCDF4.Dataset(file_prec)
+    start_date = to_datetime(int(nc_temp.variables['time'][[0]].data[0]))
+    df = pd.DataFrame(data=None, columns=["date", "latitude", "longitude", "temp", "rh", "ws", "wd", "prec"])
+    df.to_csv(csv_file, index=False)
+    for k in ['time', 'latitude', 'longitude']:
+        assert(all(nc_temp.variables[k][:].data == nc_dew.variables[k][:].data))
+        assert(all(nc_temp.variables[k][:].data == nc_wind_u.variables[k][:].data))
+        assert(all(nc_temp.variables[k][:].data == nc_wind_v.variables[k][:].data))
+        assert(all(nc_temp.variables[k][:].data == nc_prec.variables[k][:].data))
+    #result = []
+
+    for i_time, time in enumerate(nc_temp.variables['time']):
+        date = to_datetime(int(time.data))
+        print(date)
+        by_latitude = []
+        for i_lat, latitude in enumerate(nc_temp.variables['latitude']):
+            longitudes = nc_temp.variables['longitude'][:].data
+            # t = nc_temp_rh.variables['t']
+            # r = nc_temp_rh.variables['r']
+            # tp = nc_prec.variables['tp']
+            # u = nc_wind.variables['u']
+            # v = nc_wind.variables['v']
+            # temp = t[i_time, i_lat].data * t.scale_factor + t.add_offset
+            # rh = r[i_time, i_lat].data * r.scale_factor + r.add_offset
+            # prec = tp[i_time, i_lat].data * tp.scale_factor + tp.add_offset
+            # wind_u = u[i_time, i_lat].data * u.scale_factor + u.add_offset
+            # wind_v = v[i_time, i_lat].data * v.scale_factor + v.add_offset
+            def get_var(nc, name):
+                v = nc.variables[name]
+                d = v[i_time, i_lat].data
+                def fix(x):
+                    return(np.NaN if x == v.missing_value else x)
+                d = np.vectorize(fix)(d)
+                return(d)
+            temp = get_var(nc_temp, 't2m')
+            dew = get_var(nc_dew, 'd2m')
+            prec = get_var(nc_prec, 'tp')
+            u = get_var(nc_wind_u, 'u10')
+            v = get_var(nc_wind_v, 'v10')
+            df = pd.DataFrame({'longitude': longitudes,
+                               'temp': np.vectorize(kelvin_to_celcius)(temp),
+                               'rh': np.vectorize(calc_rh)(np.vectorize(kelvin_to_celcius)(dew),
+                                                           np.vectorize(kelvin_to_celcius)(temp)),
+                               'ws': np.vectorize(calc_ws)(u, v),
+                               'wd': np.vectorize(calc_wd)(u, v),
+                               # convert m to mm
+                               'prec': prec * 1000})
+            df['latitude'] = latitude
+            df = df[np.isfinite(df['temp'])]
+            df = df[np.isfinite(df['rh'])]
+            df = df[np.isfinite(df['ws'])]
+            df = df[np.isfinite(df['wd'])]
+            by_latitude.append(df)
+        df = pd.concat(by_latitude)
+        df['time'] = date
+        #result.append(df)
+        df = df[['time', 'latitude', 'longitude', 'temp', 'rh', 'ws', 'wd', 'prec']]
+        df['temp'] = df['temp'].apply(lambda x: '{:0.1f}'.format(x))
+        df['ws'] = df['ws'].apply(lambda x: '{:0.1f}'.format(x))
+        df['wd'] = df['wd'].apply(lambda x: '{:0.0f}'.format(x))
+        df['prec'] = df['prec'].apply(lambda x: '{:0.1f}'.format(x))
+        df.to_csv(csv_file, header=False, index=False, mode='a')
+
+for y in range(1980, 2021):
+    get_year(y)
